26a979a68bfcd82aab8c5d9285890381279bbc00
aysusheydayeva <aysuseydayeva01@gmail.com>
2026-04-06 00:07:37 +0400

    update

--- db.js
+++ db.js
@@ -4,7 +4,6 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "1111",
     database: "ecommerce_db"
 });
 
@@ -19,27 +18,3 @@ db.connect((err) => {
 module.exports = db; 
 
 
-const query = "SELECT*FROM categories";
-db.query (query,function(err,results){
-    if(err){
-        console.log("Xeta bash verdi: " + err.message);
-        return;
-    }
-    console.log("Kateqoriler geldi: ",results);
-});
-module.exports =db;
-
-const insertCategory = (categoryName) => {
-    const sql = "INSERT INTO categories (name) VALUES (?)";
-
-    db.query(sql, [categoryName], (err,results) =>{
-        if(err){
-            console.error("Xeta: Bazaya yazila bilmedi -", err.message);
-            return;
-        }
-        console.log("Melumat bazaya ugurla yazildi, Yeni ID:", results.insertId);
-
-    });
-};
-
-insertCategory("Meiset texnikasi");
--- server.js
+++ server.js
@@ -1,91 +1,17 @@
-const express = require('express');
+const express = require("express");
 const application = express();
 const db = require('./db');
 const PORT = 3000;
 application.use(express.json());
 
 
-application.get('/', (req, res) => {
+application.get("/", (req, res) => {
     res.send("Server və Database qoşulması uğurludur");
 });
 
 
-application.get('/categories', (req, res) => {
-    const sql = "SELECT * FROM categories"; 
 
-    db.query(sql, (err, results) => {
-        if (err) {
-            console.error("Xeta bash verdi:", err);
-            return res.status(500).json({ error: "Bazadan melumat cekile bilmedi" });
-        }
-        res.json(results);
-    });
+application.listen(PORT, () => {
+  console.log(`Server running on PORT ${PORT}`);
 });
 
-
-//application.listen(PORT, () => {
-//   console.log(`Server running on PORT ${PORT}`);
-//});
-
-
-
-/*application.post('/categories', (req,res) => {
-    const{ name } = req.body;
-
-    if(!name || name.trim() === ""){
-        return res.status(400).json({error: "kateqoriya adi bosh ola bilmez"})
-    }
-
-    const sql = "INSERT INTO categories (name) values (?)";
-
-    db.query(sql, [name],(err,results) => {
-        if(err){
-            return res.status(500).json({error: "Bazaya yazila bilmedi" });
-
-        }
-        res.status(201).json({message: "Ugurla elave edildi",id: results.insertId });
-
-    });
-});*/
-
-
-/*application.get('/products', (req,res) => {
-    const sql = "SELECT * FROM products";
-
-    db.query(sql,(err, results) => {
-        if(err) {
-            console.error("Mehsullar getirilerken xeta bash verdi:",err);
-            return res.status(500).json({error: "Mehsullar bazadan cekile bilmedi"});
-        }
-        res.json(results);
-    });
-});*/
-
-
-
-
-application.get('/products-detail', (req, res) => {
-    const sql = `
-        SELECT 
-            products.id, 
-            products.name AS product_name, 
-            products.price, 
-            categories.name AS category_name 
-        FROM products 
-        INNER JOIN categories ON products.category_id = categories.id
-    `;
-
-    db.query(sql, (err, results) => {
-        if (err) {
-            console.error("Məlumatlar birləşdirilərkən xəta:", err.message);
-            return res.status(500).json({ error: "Məlumat bazasından xəta qaytarıldı" });
-        }
-        
-        res.json(results);
-    });
-});
-
-
-application.listen(PORT,() =>{
-    console.log(`Server running on Port ${PORT}`);
-});
